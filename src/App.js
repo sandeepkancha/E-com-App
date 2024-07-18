6a5e594aef19640afa06e2403f5fe84e39cf653d
sandeepkancha <kanchasandeep0368@gmail.com>
2024-07-19 01:33:05 +0530

    removed test files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi
+        HI
       </header>
     </div>
   );
