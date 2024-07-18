05981397652978f259364849e85ad1f98d4b91de
sandeepkancha <kanchasandeep0368@gmail.com>
2024-07-19 02:20:04 +0530

    added header,footer,main pages

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter,Routes,Router } from "react-router-dom";
+
+
+const Main = () =>{
+    return(
+        <main>
+            <BrowserRouter>
+                <Routes>
+                    <Route path/>
+
+                </Routes>
+            </BrowserRouter>
+        </main>
+    )
+}
